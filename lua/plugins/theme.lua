0dc667a3e5ac342721b7a7031e527bd49cf80fdd
Harry Vince <harryavince@gmail.com>
2023-12-11 19:11:24 +0000

    fix catppuccin

--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -1,31 +1,31 @@
 return {
-    "catppuccin/nvim",
-    name = "catppuccin",
-    priority = 1000,
-    config = function()
-        require("catppuccin").setup({
-            flavour = "macchiato",     -- latte, frappe, macchiato, mocha
-            transparent_background = false,
-            show_end_of_buffer = false,
-            term_colors = true,
-            dim_inactive = {
-                enabled = true,
-            },
-            integrations = {
-                gitsigns = true,
+	"catppuccin/nvim",
+	name = "catppuccin",
+	priority = 1000,
+	config = function()
+		require("catppuccin").setup({
+			flavour = "macchiato", -- latte, frappe, macchiato, mocha
+			transparent_background = true,
+			show_end_of_buffer = false,
+			term_colors = true,
+			dim_inactive = {
+				enabled = false,
+			},
+			integrations = {
+				gitsigns = true,
                 telescope = {
                     enabled = true,
                 },
-                noice = true,
-                cmp = true,
-                harpoon = true,
-                mason = true,
-                treesitter_context = true,
-                treesitter = true,
-                dap = true,
-                dap_ui = true,
-            },
-        })
-        vim.cmd.colorscheme "catppuccin"
-    end
+				noice = true,
+				cmp = true,
+				harpoon = true,
+				mason = true,
+				treesitter_context = true,
+				treesitter = true,
+				dap = true,
+				dap_ui = true,
+			},
+		})
+		vim.cmd.colorscheme("catppuccin")
+	end,
 }
