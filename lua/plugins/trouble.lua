1c1de5c32f8e09d4f2a92eee8f2c0fb7887a930b
Harry Vince <harryavince@gmail.com>
2024-01-07 12:07:32 +0000

    feat: remove noice add trouble

---
+++ lua/plugins/trouble.lua
@@ -0,0 +1,10 @@
+return {
+    "folke/trouble.nvim",
+    config = function ()
+        local trouble = require("trouble")
+
+        vim.keymap.set("n", "<leader>tt", function ()
+            trouble.toggle("workspace_diagnostics")
+        end)
+    end
+}
