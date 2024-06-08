c3c878a9aa66b12ec09e5db1144f3376a5e3249d
Harry Vince <harryavince@gmail.com>
2024-06-08 20:09:44 +0100

    feat: add floaterm back

---
+++ lua/plugins/floaterm.lua
@@ -0,0 +1,10 @@
+return {
+    'voldikss/vim-floaterm',
+    config = function ()
+        local options = { noremap = true, silent = true }
+
+        vim.keymap.set('n', '<leader>ld', '<CMD>FloatermNew --autoclose=2 --height=0.9 --width=0.9 lazydocker<CR>', options)
+        vim.keymap.set('n', '<leader>lg', '<CMD>FloatermNew --autoclose=2 --height=0.9 --width=0.9 lazygit<CR>', options)
+        vim.keymap.set('n', '<leader>tt', '<CMD>FloatermNew --autoclose=2 --height=0.9 --width=0.9 zsh<CR>', options)
+    end
+}
