143d873813d66574f2aaf1ce5be401c050d9517a
Harry Vince <harryavince@gmail.com>
2024-08-22 20:25:42 +0100

    feat: folke with the comments - also commenting is native to neovim now

--- lua/plugins/comment.lua
+++ lua/plugins/comment.lua
@@ -1,7 +1,8 @@
 return {
-    "numToStr/Comment.nvim",
-    lazy = false,
-    config = function ()
-        require("Comment").setup()
-    end
+	{
+		"folke/ts-comments.nvim",
+		opts = {},
+		event = "VeryLazy",
+		enabled = vim.fn.has("nvim-0.10.0") == 1,
+	},
 }
