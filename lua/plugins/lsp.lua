d718091d37b77559407feebafc35dad0103eda0a
Harry Vince <harryavince@gmail.com>
2024-11-09 23:36:05 +0000

    refactor: lsp config

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -11,57 +11,46 @@ return {
 		local lspconfig = require("lspconfig")
 		local cmp_nvim_lsp = require("cmp_nvim_lsp")
 
-		local servers = {
-			"ts_ls",
-			"html",
-			"cssls",
-			"svelte",
-			"lua_ls",
-			"pyright",
-			"bashls",
-			"jsonls",
-			"ansiblels",
-		}
-
-		local disable_formatting = {
-			"ts_ls",
-			"html",
-			"cssls",
-			"svelte",
-			"lua_ls",
-			"bashls",
-			"jsonls",
-		}
-
-		local capabilities = cmp_nvim_lsp.default_capabilities()
-		local on_attach = function(client, _)
-			for _, value in ipairs(disable_formatting) do
-				if client.name == value then
-					client.server_capabilities.documentFormattingProvider = false
-					client.server_capabilities.documentFormattingRangeProvider = false
-				end
-			end
-
-			if client.name == "svelte" then
-				vim.api.nvim_create_autocmd("BufWritePost", {
-					pattern = { "*.js", "*.ts" },
-					callback = function(ctx)
-						if client.name == "svelte" then
-							client.notify("$/onDidChangeTsOrJsFile", { uri = ctx.file })
+		local configure = function(server_name)
+			local opts = {
+				on_attach = function(client, _)
+					-- Disable formatting for the following servers
+					for _, value in ipairs({
+						"ts_ls",
+						"html",
+						"cssls",
+						"svelte",
+						"lua_ls",
+						"bashls",
+						"jsonls",
+						"nil_ls",
+					}) do
+						if client.name == value then
+							client.server_capabilities.documentFormattingProvider = false
+							client.server_capabilities.documentFormattingRangeProvider = false
 						end
-					end,
-				})
+					end
 
-				vim.api.nvim_create_autocmd({ "BufWrite" }, {
-					group = vim.api.nvim_create_augroup("svelte_ondidchangetsorjsfile", { clear = true }),
-					pattern = { "+page.server.ts", "+page.ts", "+layout.server.ts", "+layout.ts" },
-					command = 'lua require("config.utils").svelteFix()',
-				})
-			end
-		end
+					if client.name == "svelte" then
+						vim.api.nvim_create_autocmd("BufWritePost", {
+							pattern = { "*.js", "*.ts" },
+							callback = function(ctx)
+								if client.name == "svelte" then
+									client.notify("$/onDidChangeTsOrJsFile", { uri = ctx.file })
+								end
+							end,
+						})
 
-		local setup_lsp = function(server_name)
-			local opts = { on_attach = on_attach, capabilities = capabilities }
+						vim.api.nvim_create_autocmd({ "BufWrite" }, {
+							group = vim.api.nvim_create_augroup("svelte_ondidchangetsorjsfile", { clear = true }),
+							pattern = { "+page.server.ts", "+page.ts", "+layout.server.ts", "+layout.ts" },
+							command = 'lua require("config.utils").svelteFix()',
+						})
+					end
+				end,
+				capabilities = cmp_nvim_lsp.default_capabilities(),
+			}
+
 			if server_name == "lua_ls" then
 				opts.settings = {
 					Lua = {
@@ -88,16 +77,26 @@ return {
 
 		require("mason").setup()
 		require("mason-lspconfig").setup({
-			ensure_installed = servers,
-			automatic_installation = true,
+			ensure_installed = {
+				"ts_ls",
+				"html",
+				"cssls",
+				"svelte",
+				"lua_ls",
+				"pyright",
+				"bashls",
+				"jsonls",
+				"ansiblels",
+			},
+			automatic_installation = false,
 			handlers = {
 				function(server_name)
-					lspconfig[server_name].setup(setup_lsp(server_name))
+					lspconfig[server_name].setup(configure(server_name))
 				end,
 			},
 		})
 
-        -- Globally installed servers or not managed by mason
-        lspconfig.nil_ls.setup{}
+		-- Globally installed servers or not managed by mason
+		lspconfig.nil_ls.setup(configure("nil_ls"))
 	end,
 }
