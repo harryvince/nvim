31514420b18c09e98fc9a3ba86ba48a8ee7035ca
Harry Vince <harryavince@gmail.com>
2024-12-01 21:40:23 +0000

    feat: change higlighting of floats and cmp stuff

--- lua/plugins/completion.lua
+++ lua/plugins/completion.lua
@@ -34,8 +34,15 @@ return {
 			color_square_width = 2,
 		})
 
+        local colour = "#2A2F37"
+		vim.api.nvim_set_hl(0, "CmpNormal", { bg = colour })
+		vim.api.nvim_set_hl(0, "NormalFloat", { bg = colour })
 		local cmp = require("cmp")
 		cmp.setup({
+			window = {
+				completion = { winhighlight = "Normal:CmpNormal" },
+				documentation = { winhighlight = "Normal:CmpNormal" },
+			},
 			sources = {
 				{
 					name = "lazydev",
@@ -49,6 +56,8 @@ return {
 			mapping = {
 				["<C-n>"] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Insert }),
 				["<C-p>"] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Insert }),
+				["<C-d>"] = cmp.mapping.scroll_docs(4),
+				["<C-u>"] = cmp.mapping.scroll_docs(-4),
 				["<C-Space>"] = cmp.mapping(
 					cmp.mapping.confirm({
 						behavior = cmp.ConfirmBehavior.Insert,
