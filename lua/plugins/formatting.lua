9aa7cfcefc485c62daec7e94e2a9ec18efd449f1
Harry Vince <harryavince@gmail.com>
2024-12-08 15:38:59 +0000

    feat: setup shell formatting

--- lua/plugins/formatting.lua
+++ lua/plugins/formatting.lua
@@ -7,10 +7,11 @@ return {
 		conform.setup({
 			formatters_by_ft = {
 				lua = { "stylua" },
-				javascript = { "prettier" },
+				javascript = { "prettier", stop_after_first = true },
 				typescript = { "prettier", stop_after_first = true },
 				yaml = { "prettier" },
 				jsonls = { "prettier" },
+                sh = { "shfmt" },
 			},
 		})
 
