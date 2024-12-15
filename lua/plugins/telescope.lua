11c35234e921c9a96c1182f19d593ae87dccd3ff
Harry Vince <harryavince@gmail.com>
2024-12-15 21:43:49 +0000

    style: change the layout for telescope

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -9,33 +9,26 @@ return {
 	},
 	event = "VeryLazy",
 	config = function()
+		local picker_opts = { theme = "ivy" }
+
 		require("telescope").setup({
-			defaults = {
-				layout_config = {
-					horizontal = {
-						prompt_position = "top",
-						preview_width = 0.55,
-						results_width = 0.8,
-					},
-					vertical = {
-						mirror = false,
-					},
-					width = 0.85,
-					height = 0.92,
-					preview_cutoff = 120,
-				},
-			},
 			extensions = {
 				fzf = {},
+			},
+			pickers = {
+				find_files = picker_opts,
+				git_files = picker_opts,
+				grep_string = picker_opts,
+				live_grep = picker_opts,
 			},
 		})
 
 		require("telescope").load_extension("fzf")
 
 		local builtin = require("telescope.builtin")
-		vim.keymap.set("n", "<leader>pf", builtin.find_files, {})
-		vim.keymap.set("n", "<leader>bf", builtin.buffers, {})
-		vim.keymap.set("n", "<C-p>", builtin.git_files, {})
-		vim.keymap.set("n", "<leader>ps", builtin.live_grep, {})
+		vim.keymap.set("n", "<leader>pf", builtin.find_files)
+		vim.keymap.set("n", "<leader>bf", builtin.buffers)
+		vim.keymap.set("n", "<C-p>", builtin.git_files)
+		vim.keymap.set("n", "<leader>ps", builtin.live_grep)
 	end,
 }
