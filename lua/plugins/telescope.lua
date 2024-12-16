b4f28c390721d8b984b22766594e9b128405acdb
Harry Vince <harryavince@gmail.com>
2024-12-16 19:38:53 +0000

    fix: telescope

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -5,19 +5,32 @@ return {
 		{ "nvim-lua/plenary.nvim" },
 		{ "nvim-tree/nvim-web-devicons" },
 		{ "aznhe21/actions-preview.nvim" },
+		{ "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
 	},
 	event = "VeryLazy",
 	config = function()
-		local picker_opts = { theme = "ivy" }
-
 		require("telescope").setup({
-			pickers = {
-				find_files = picker_opts,
-				git_files = picker_opts,
-				grep_string = picker_opts,
-				live_grep = picker_opts,
+			defaults = {
+				layout_config = {
+					horizontal = {
+						prompt_position = "top",
+						preview_width = 0.55,
+						results_width = 0.8,
+					},
+					vertical = {
+						mirror = false,
+					},
+					width = 0.85,
+					height = 0.92,
+					preview_cutoff = 120,
+				},
+			},
+			extensions = {
+				fzf = {},
 			},
 		})
+
+		require("telescope").load_extension("fzf")
 
 		local builtin = require("telescope.builtin")
 		vim.keymap.set("n", "<leader>pf", builtin.find_files)
