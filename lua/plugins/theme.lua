9f4b780e08a1fda2a87234f1162a864b913ab86d
Harry Vince <harryavince@gmail.com>
2024-12-17 21:10:37 +0000

    feat: update colorscheme and telescope

--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -5,7 +5,7 @@ return {
 	config = function()
 		require("catppuccin").setup({
 			flavour = "mocha", -- latte, frappe, macchiato, mocha
-			transparent_background = true,
+			transparent_background = false,
 			show_end_of_buffer = false,
 			term_colors = true,
 			dim_inactive = {
@@ -26,17 +26,20 @@ return {
 				dap = true,
 				dap_ui = true,
 			},
+			color_overrides = {
+				mocha = {
+					base = "#000000",
+					mantle = "#000000",
+					crust = "#000000",
+				},
+			},
 			custom_highlights = function(colors)
 				return {
-					LineNr = { fg = colors.yellow },
+					-- LineNr = { fg = colors.yellow },
 					GitSignsCurrentLineBlame = { fg = colors.flamingo },
 				}
 			end,
 		})
 		vim.cmd.colorscheme("catppuccin")
-
-		local colour = "#1e1e1e"
-		vim.api.nvim_set_hl(0, "CmpNormal", { bg = colour })
-		vim.api.nvim_set_hl(0, "CursorLine", { bg = "#000000" })
 	end,
 }
