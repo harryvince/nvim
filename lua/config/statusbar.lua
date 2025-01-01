6a1a3137c8cdb944ebf80bab699bd75311ed5e98
Harry Vince <harryavince@gmail.com>
2025-01-01 13:09:30 +0000

    fix: dont display lsp and formatter on netrw

--- lua/config/statusbar.lua
+++ lua/config/statusbar.lua
@@ -1,4 +1,8 @@
 function LspStatus()
+    if vim.bo.filetype == "netrw" then
+        return ""
+    end
+
 	local clients = vim.lsp.get_clients()
 	local buf_clients = {}
 
@@ -9,13 +13,17 @@ function LspStatus()
 	end
 
 	if #buf_clients == 0 then
-		return "None"
+		return "LSP: None"
 	end
 
-	return table.concat(buf_clients, ", ")
+	return "LSP: " .. table.concat(buf_clients, ", ") .. " "
 end
 
 function FormatterStatus()
+    if vim.bo.filetype == "netrw" then
+        return ""
+    end
+
 	local conform = require("conform")
 	local bufnr = vim.api.nvim_get_current_buf()
 	local formatters, _ = conform.list_formatters_to_run(bufnr)
@@ -25,9 +33,9 @@ function FormatterStatus()
 		for _, formatter in ipairs(formatters) do
 			table.insert(formatter_names, formatter.name)
 		end
-        return table.concat(formatter_names, ", ")
+        return "FMT: " .. table.concat(formatter_names, ", ") .. " |"
     else
-        return "None"
+        return "FMT: None"
 	end
 end
 
@@ -36,7 +44,7 @@ vim.o.statusline = "%f" -- File name
 	.. " %r" -- Read-only flag
 	.. " %y" -- File type
 	.. " %= " -- Right-aligned section
-	.. "LSP: %{v:lua.LspStatus()} " -- Custom LSP function
-	.. "FMT: %{v:lua.FormatterStatus()} |" -- Custom LSP function
+	.. "%{v:lua.LspStatus()} " -- Custom LSP function
+	.. "%{v:lua.FormatterStatus()}" -- Custom LSP function
 	.. " %p%%" -- Percentage through the file
 	.. " %l:%c" -- Line and column
