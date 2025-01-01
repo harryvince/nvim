d36a838795fed051a56b95bf666b8e23eeda6e04
Harry Vince <harryavince@gmail.com>
2025-01-01 01:27:26 +0000

    feat: update status bar to display formatter

--- lua/config/statusbar.lua
+++ lua/config/statusbar.lua
@@ -9,18 +9,34 @@ function LspStatus()
 	end
 
 	if #buf_clients == 0 then
-		return "No LSP"
+		return "None"
 	end
 
 	return table.concat(buf_clients, ", ")
 end
 
+function FormatterStatus()
+	local conform = require("conform")
+	local bufnr = vim.api.nvim_get_current_buf()
+	local formatters, _ = conform.list_formatters_to_run(bufnr)
+
+	if #formatters > 0 then
+		local formatter_names = {}
+		for _, formatter in ipairs(formatters) do
+			table.insert(formatter_names, formatter.name)
+		end
+        return table.concat(formatter_names, ", ")
+    else
+        return "None"
+	end
+end
 
 vim.o.statusline = "%f" -- File name
 	.. " %m" -- Modified flag
 	.. " %r" -- Read-only flag
 	.. " %y" -- File type
 	.. " %= " -- Right-aligned section
-	.. "LSP: %{v:lua.LspStatus()}" -- Custom LSP function
+	.. "LSP: %{v:lua.LspStatus()} " -- Custom LSP function
+	.. "FMT: %{v:lua.FormatterStatus()} |" -- Custom LSP function
 	.. " %p%%" -- Percentage through the file
 	.. " %l:%c" -- Line and column
