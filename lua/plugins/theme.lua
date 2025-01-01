1559a9726e532d727eb02172308dc7bda19523df
Harry Vince <harryavince@gmail.com>
2025-01-01 17:08:40 +0000

    feat: swap back to rose-pine theme

--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -1,45 +1,19 @@
 return {
-	"catppuccin/nvim",
-	name = "catppuccin",
+	"rose-pine/neovim",
+	name = "rose-pine",
 	priority = 1000,
 	config = function()
-		require("catppuccin").setup({
-			flavour = "mocha", -- latte, frappe, macchiato, mocha
-			transparent_background = false,
-			show_end_of_buffer = false,
-			term_colors = true,
-			dim_inactive = {
-				enabled = false,
+		---@diagnostic disable-next-line: missing-fields
+		require("rose-pine").setup({
+			variant = "moon",
+			styles = {
+				bold = false,
+				italic = false,
+				transparency = true,
 			},
-			integrations = {
-				gitsigns = true,
-				telescope = {
-					enabled = true,
-					-- style = "nvchad",
-				},
-				noice = false,
-				cmp = true,
-				harpoon = true,
-				mason = true,
-				treesitter_context = true,
-				treesitter = true,
-				dap = true,
-				dap_ui = true,
-			},
-			-- color_overrides = {
-			-- 	mocha = {
-			-- 		base = "#000000",
-			-- 		mantle = "#000000",
-			-- 		crust = "#000000",
-			-- 	},
-			-- },
-			custom_highlights = function(colors)
-				return {
-					LineNr = { fg = colors.overlay2 },
-					GitSignsCurrentLineBlame = { fg = colors.flamingo },
-				}
-			end,
 		})
-		vim.cmd.colorscheme("catppuccin")
+
+		vim.cmd.colorscheme("rose-pine")
+        vim.opt.fillchars = { eob = " " }
 	end,
 }
