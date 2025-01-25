5217fe47bc7f376faa2b966e0402fd651b3a5607
Harry Vince <harryavince@gmail.com>
2025-01-25 18:27:37 +0000

    disable avante

--- lua/plugins/avante.lua
+++ lua/plugins/avante.lua
@@ -5,11 +5,12 @@ return {
 	version = false,
 	opts = {
 		buffer = { modifiable = true },
-		patch = { auto_apply = true	},
+		patch = { auto_apply = true },
 	},
 	cond = function()
 		return vim.uv.os_gethostname() == "HV-MBP.local"
 	end,
+	enabled = false,
 	build = "make",
 	dependencies = {
 		"stevearc/dressing.nvim",
