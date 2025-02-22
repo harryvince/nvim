32a7ab57135ba7b99740689f318b8baf37207a0c
Harry Vince <harryavince@gmail.com>
2025-02-22 12:42:03 +0000

    supermaven suggestions are only accepted on shift+tab

--- lua/plugins/supermaven.lua
+++ lua/plugins/supermaven.lua
@@ -4,6 +4,10 @@ return {
 		return vim.uv.os_gethostname() == "HV-MBP.local"
 	end,
 	config = function()
-		require("supermaven-nvim").setup({})
+		require("supermaven-nvim").setup({
+			keymaps = {
+				accept_suggestion = "<S-Tab>",
+			},
+		})
 	end,
 }
