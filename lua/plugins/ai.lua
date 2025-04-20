07361969a12531887386092be666caa87a7758b0
Harry Vince <harryavince@gmail.com>
2025-04-20 21:41:25 +0100

    refactor how i check if the machine is my home machine or not

--- lua/plugins/ai.lua
+++ lua/plugins/ai.lua
@@ -1,9 +1,7 @@
 return {
 	{
 		"supermaven-inc/supermaven-nvim",
-		cond = function()
-			return vim.uv.os_gethostname() == "HV-MBP.local"
-		end,
+		cond = require("config.utils").is_personal,
 		opts = {
 			keymaps = {
 				accept_suggestion = "<S-Tab>",
@@ -13,18 +11,13 @@ return {
 	{
 		"frankroeder/parrot.nvim",
 		dependencies = { "ibhagwan/fzf-lua", "nvim-lua/plenary.nvim" },
-		cond = function()
-			return vim.uv.os_gethostname() == "HV-MBP.local"
-		end,
-		config = function()
-			require("parrot").setup({
-				-- Providers must be explicitly added to make them available.
-				providers = {
-					anthropic = {
-						api_key = os.getenv("ANTHROPIC_API_KEY"),
-					},
+		cond = require("config.utils").is_personal,
+		opts = {
+			providers = {
+				anthropic = {
+					api_key = os.getenv("ANTHROPIC_API_KEY"),
 				},
-			})
-		end,
+			},
+		},
 	},
 }
