09b6eb161dd8f3f01a51a417548958ab75298878
Harry Vince <harryavince@gmail.com>
2025-04-20 14:41:12 +0100

    use native lazygit plugin over floaterm

--- lua/plugins/floaterm.lua
+++
@@ -1,9 +0,0 @@
-return {
-    'voldikss/vim-floaterm',
-    config = function ()
-        local options = { noremap = true, silent = true }
-
-        vim.keymap.set('n', '<leader>ld', '<CMD>FloatermNew --autoclose=2 --height=0.9 --width=0.9 lazydocker<CR>', options)
-        vim.keymap.set('n', '<leader>lg', '<CMD>FloatermNew --autoclose=2 --height=0.9 --width=0.9 lazygit<CR>', options)
-    end
-}
