8c0249acd496e06f3379f157557cfbd5e816e378
Harry Vince <harryavince@gmail.com>
2025-04-20 22:16:06 +0100

    use my fork of vesper till hopefully the initial pr for telescope is merged

--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -1,6 +1,7 @@
 return {
 	{
-		"datsfilipe/vesper.nvim",
+		"harryvince/vesper.nvim",
+		dev = false,
 		config = function()
 			require("vesper").setup({
 				transparent = false,
@@ -17,7 +18,9 @@ return {
 			vim.cmd.colorscheme("vesper")
 
 			vim.opt.fillchars = { eob = " " }
-			vim.api.nvim_set_hl(0, "TelescopeSelection", { bg = "#292925", fg = "#ffffff", bold = true })
+			-- vim.api.nvim_set_hl(0, "TelescopeSelection", { bg = "#292925", fg = "#ffffff", bold = true })
+			-- vim.api.nvim_set_hl(0, "TelescopeTitle", { bg = "#ffcfa8", fg = "#101010", bold = true })
+			-- vim.api.nvim_set_hl(0, "TelescopeMatching", { fg = "#ffcfa8" })
 		end,
 	},
 }
