53478680ed10d335e11f5cd709e84b46ea8a1b5b
Harry Vince <harryavince@gmail.com>
2025-09-07 14:07:12 +0100

    update types on pre-commit config

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -69,7 +69,7 @@ return {
 								["http://json.schemastore.org/github-workflow"] = ".github/workflows/*",
 								["https://raw.githubusercontent.com/compose-spec/compose-spec/master/schema/compose-spec.json"] = "*docker-compose*.{yml,yaml}",
 								["https://gitlab.com/gitlab-org/gitlab/-/raw/master/app/assets/javascripts/editor/schema/ci.json"] = "*gitlab-ci*.{yml,yaml}",
-								["https://json.schemastore.org/pre-commit-config.json"] = ".pre-commit-config.yml",
+								["https://json.schemastore.org/pre-commit-config.json"] = ".pre-commit-config.{yml,yaml}",
 								["https://raw.githubusercontent.com/F1bonacc1/process-compose/refs/heads/main/schemas/process-compose-schema.json"] = "process-compose.yaml",
 							},
 						},
