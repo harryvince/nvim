0247df5cfaa3c8fefeff60ce568bfd515699733f
Harry Vince <harryavince@gmail.com>
2025-09-28 21:23:34 +0100

    so svelte lsp is fixed now and dont need this anymore

--- lua/config/utils.lua
+++ lua/config/utils.lua
@@ -1,15 +1,5 @@
 local M = {}
 
-function M.svelteFix()
-	local clients = vim.lsp.get_active_clients()
-	for _, found_client in ipairs(clients) do
-		if found_client.name == "svelte" then
-			vim.cmd("LspRestart " .. found_client.id)
-			break
-		end
-	end
-end
-
 function M.TransparentBackground()
 	vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
 	vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
