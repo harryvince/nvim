46313f6c68513498235f12050c595ebba24598fd
Harry Vince <harryavince@gmail.com>
2025-09-28 11:27:23 +0100

    fix typescript server and remove deno since i never use it

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -40,15 +40,7 @@ return {
 				dockerls = true,
 				lua_ls = { server_capabilities = { semanticTokensProvider = vim.NIL } },
 				biome = { root_dir = lspconfig.util.root_pattern("biome.json") },
-				ts_ls = {
-					root_dir = lspconfig.util.root_pattern("package.json"),
-					single_file_support = false,
-					server_capabilities = { documentFormattingProvider = false },
-				},
-				denols = {
-					manual_install = true,
-					root_dir = lspconfig.util.root_pattern("deno.json", "deno.jsonc"),
-				},
+				ts_ls = { server_capabilities = { documentFormattingProvider = false } },
 				jsonls = {
 					server_capabilities = { documentFormattingProvider = false },
 					settings = {
