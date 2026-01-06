546fa6d2bc8967419c925b28c855b6d4c59385e1
Harry Vince <harryavince@gmail.com>
2026-01-06 14:01:49 +0000

    finally got my completion to where i want it to be back in cmp days

--- lua/plugins/completion.lua
+++ lua/plugins/completion.lua
@@ -1,38 +1,61 @@
+local function map_providers()
+  local providers = {
+    lsp = {},
+    buffer = {},
+    snippets = {},
+    path = { opts = { show_hidden_files_by_default = true } },
+  }
+  for name, provider in pairs(providers) do
+    local extra_opts = {
+      ---@diagnostic disable-next-line: unused-local
+      transform_items = function(ctx, items)
+        for _, item in ipairs(items) do
+          item.kind_icon = "[" .. name .. "]"
+        end
+        return items
+      end,
+    }
+    local provider_opts = vim.tbl_deep_extend("force", provider, extra_opts)
+    providers[name] = provider_opts
+  end
+  return providers
+end
+
 return {
-	{
-		"saghen/blink.cmp",
-		dependencies = "rafamadriz/friendly-snippets",
-		version = "v1.*",
-		opts = {
-			keymap = { preset = "default" },
+  {
+    "saghen/blink.cmp",
+    dependencies = "rafamadriz/friendly-snippets",
+    version = "v1.*",
+    opts = {
+      keymap = { preset = "default" },
 
-			appearance = {
-				use_nvim_cmp_as_default = true,
-				nerd_font_variant = "mono",
-			},
+      appearance = {
+        use_nvim_cmp_as_default = true,
+        nerd_font_variant = "mono",
+      },
 
-			signature = { enabled = true },
+      signature = { enabled = true },
 
-			sources = {
-				providers = {
-					path = {
-						opts = { show_hidden_files_by_default = true },
-					},
-				},
-			},
+      sources = {
+        providers = map_providers(),
+      },
 
-			completion = {
-				menu = {
-					auto_show = function(ctx)
-						return ctx.mode ~= "cmdline"
-					end,
-				},
-			},
-		},
-	},
-	{
-		"supermaven-inc/supermaven-nvim",
-		cond = require("config.utils").is_personal,
-		opts = { keymaps = { accept_suggestion = "<S-Tab>" } },
-	},
+      completion = {
+        menu = {
+          auto_show = function(ctx)
+            return ctx.mode ~= "cmdline"
+          end,
+          draw = { columns = {
+          { "label", "label_description", gap = 1 },
+          { "kind_icon" },
+          } }
+        },
+      },
+    },
+  },
+  {
+    "supermaven-inc/supermaven-nvim",
+    cond = require("config.utils").is_personal,
+    opts = { keymaps = { accept_suggestion = "<S-Tab>" } },
+  },
 }
--- lua/plugins/formatting.lua
+++ lua/plugins/formatting.lua
@@ -1,48 +1,48 @@
 return {
-	"stevearc/conform.nvim",
-	event = { "BufReadPre", "BufNewFile" },
-	config = function()
-		local conform = require("conform")
-		local util = require("conform.util")
+  "stevearc/conform.nvim",
+  event = { "BufReadPre", "BufNewFile" },
+  config = function()
+    local conform = require("conform")
+    local util = require("conform.util")
 
-		vim.g.formatOnSave = true
+    vim.g.formatOnSave = true
 
-		local function get_formatter()
-			if util.root_file("biome.json") then
-				return { "biome" }
-			else
-				return { "prettier" }
-			end
-		end
+    local function get_formatter()
+      if util.root_file("biome.json") then
+        return { "biome" }
+      else
+        return { "prettier" }
+      end
+    end
 
-		conform.setup({
-			formatters_by_ft = {
-				lua = { "stylua" },
-				javascript = get_formatter(),
-				typescript = get_formatter(),
-				typescriptreact = get_formatter(),
-				javascriptreact = get_formatter(),
-				yaml = { "prettier" },
-				json = { "prettier" },
-				markdown = { "prettier" },
-				sh = { "shfmt" },
-				py = { "ruff" },
-				go = { "gofmt" },
-			},
-			format_after_save = function()
-				if vim.g.formatOnSave == true then
-					conform.format({
-						lsp_fallback = true,
-                        quiet = true,
-						timeout_ms = 5000,
-					})
-				end
-			end,
-		})
+    conform.setup({
+      formatters_by_ft = {
+        lua = { "stylua" },
+        javascript = get_formatter(),
+        typescript = get_formatter(),
+        typescriptreact = get_formatter(),
+        javascriptreact = get_formatter(),
+        yaml = { "prettier" },
+        json = { "prettier" },
+        markdown = { "prettier" },
+        sh = { "shfmt" },
+        py = { "ruff" },
+        go = { "gofmt" },
+      },
+      format_after_save = function()
+        if vim.g.formatOnSave == true then
+          conform.format({
+            lsp_fallback = true,
+            quiet = true,
+            timeout_ms = 5000,
+          })
+        end
+      end,
+    })
 
-		vim.keymap.set("n", "<leader>ff", function()
-			vim.g.formatOnSave = not vim.g.formatOnSave
-			print("Format on save => " .. tostring(vim.g.formatOnSave))
-		end)
-	end,
+    vim.keymap.set("n", "<leader>ff", function()
+      vim.g.formatOnSave = not vim.g.formatOnSave
+      print("Format on save => " .. tostring(vim.g.formatOnSave))
+    end)
+  end,
 }
