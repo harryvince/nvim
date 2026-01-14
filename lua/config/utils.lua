0b2e99a79cafc27fae1005f867d0b27a7c6cdacc
Harry Vince <harryavince@gmail.com>
2026-01-14 21:02:57 +0000

    add option to disable autocomplete

--- lua/config/utils.lua
+++ lua/config/utils.lua
@@ -1,12 +1,12 @@
 local M = {}
 
 function M.TransparentBackground()
-	vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-	vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+  vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+  vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
 end
 
 function M.is_personal()
-	return vim.uv.os_gethostname() == "Harrys-MacBook-Pro.local"
+  return vim.uv.os_gethostname() == "Harrys-MacBook-Pro.local" and not vim.g.disable_autocomplete
 end
 
 return M
