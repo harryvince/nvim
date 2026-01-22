65653a65a02c28b7a48a973bcdd09fa18cb0dbda
Harry Vince <harryavince@gmail.com>
2026-01-22 21:38:05 +0000

    add icon to the status bar when auto formatting is turned off

--- lua/plugins/statusbar.lua
+++ lua/plugins/statusbar.lua
@@ -1,21 +1,29 @@
 return {
-	"nvim-lualine/lualine.nvim",
-	config = function()
-		require("lualine").setup({
-			options = {
-				icons_enabled = false,
-				theme = "auto",
-				component_separators = "",
-				section_separators = "",
-			},
-			sections = {
-				lualine_a = { "" },
-				lualine_b = { { "filename", path = 1 } },
-				lualine_c = { "diagnostics" },
-				lualine_x = { "filetype" },
-				lualine_y = { "progress" },
-				lualine_z = { "location" },
-			},
-		})
-	end,
+  "nvim-lualine/lualine.nvim",
+  config = function()
+    local function formatting()
+      if not vim.g.formatOnSave then
+        return "󰷙"
+      else
+        return ""
+      end
+    end
+
+    require("lualine").setup({
+      options = {
+        icons_enabled = false,
+        theme = "auto",
+        component_separators = "",
+        section_separators = "",
+      },
+      sections = {
+        lualine_a = { "" },
+        lualine_b = { { "filename", path = 1 }, formatting },
+        lualine_c = { "diagnostics" },
+        lualine_x = { "filetype" },
+        lualine_y = { "progress" },
+        lualine_z = { "location" },
+      },
+    })
+  end,
 }
