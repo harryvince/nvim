145868d963301daf1aa1a183e7e610966a78f98e
Harry Vince <harryavince@gmail.com>
2026-06-18 18:26:09 +0100

    speed up conform auto formatting hopefully

--- lua/plugins/formatting.lua
+++ lua/plugins/formatting.lua
@@ -3,10 +3,12 @@ return {
   event = { "BufReadPre", "BufNewFile" },
   config = function()
     local conform = require("conform")
-    local util = require("conform.util")
 
-    local function get_formatter()
-      if util.root_file("biome.json") then
+    local function get_formatter(bufnr)
+      local bufname = vim.api.nvim_buf_get_name(bufnr)
+      local dirname = bufname ~= "" and vim.fs.dirname(bufname) or vim.uv.cwd()
+
+      if vim.fs.root(dirname, "biome.json") then
         return { "biome" }
       else
         return { "oxfmt", "prettier", stop_after_first = true }
@@ -16,10 +18,10 @@ return {
     conform.setup({
       formatters_by_ft = {
         lua = { "stylua" },
-        javascript = get_formatter(),
-        typescript = get_formatter(),
-        typescriptreact = get_formatter(),
-        javascriptreact = get_formatter(),
+        javascript = get_formatter,
+        typescript = get_formatter,
+        typescriptreact = get_formatter,
+        javascriptreact = get_formatter,
         yaml = { "oxfmt" },
         toml = { "oxfmt" },
         json = { "oxfmt" },
@@ -30,11 +32,11 @@ return {
       },
       format_on_save = function()
         if vim.g.formatOnSave == true then
-          conform.format({
-            lsp_fallback = true,
+          return {
+            lsp_format = "fallback",
             quiet = true,
-            timeout_ms = 5000,
-          })
+            timeout_ms = 1000,
+          }
         end
       end,
     })
